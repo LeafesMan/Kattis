0b30ee713baef22a4304744600980746eab5219f
Ian Ford <89503834+IanTFord@users.noreply.github.com>
2022-02-01 18:13:22 -0500

    Added 4Thought solution and Solver class

--- Kattis/src/FourThought.java
+++ Kattis/src/FourThought.java
@@ -14,29 +14,51 @@ public class FourThought {
 			for(int j = 0; j < 4; j ++) {
 				for(int k = 0; k < 4; k++) {
 					//Generate Solution
-					result = GetOperator(i, result);
-					equation = GetOperator(i, equation);
-					result = GetOperator(j, result);
-					equation = GetOperator(j, equation);
-					result = GetOperator(k, result);
-					equation = GetOperator(k, equation);
+					equation = GetEquation(i,j,k);
+					result = Solver.evaluate(equation);
 					
 					//Add solution to HashMap
-					equation = equation + " = " + result;
 					solutions.put(result, equation);
 				}
 			}
 		}
 		
 		//Input and Output
-		while(in.hasNextLine()) {
-			String sol = (solutions.get(Integer.parseInt(in.nextLine())) == null) ? "no solution"   : solutions.get(Integer.parseInt(in.nextLine()));
-			System.out.println(sol);
+		int i = 0;
+		int numLines = Integer.parseInt(in.nextLine());
+		while(i < numLines) {
+			int sol = Integer.parseInt(in.nextLine());
+			String eq = solutions.get(sol);
+			System.out.println( (eq == null) ? "no solution" : eq + " = " + sol);
+			i++;
 		}
 	}
-
+	
+	
+	//Solves the equation Multiplying and dividing first
+	public static int SolveEquation(int i, int j, int k) {
+		int num = 4;
+		if (i < 2) num = ParseOperator(i, num);
+		if (j < 2) num = ParseOperator(j, num);
+		if (k < 2) num = ParseOperator(k, num);
+		if (i > 1) num = ParseOperator(i, num);
+		if (j > 1) num = ParseOperator(j, num);
+		if (k > 1) num = ParseOperator(k, num);
+		return num;
+	}
+	
+	//Returns the string equation
+	public static String GetEquation(int i, int j, int k) {
+		String eq = "4";
+		eq = ParseOperator(i, eq);
+		eq = ParseOperator(j, eq);
+		eq = ParseOperator(k, eq);
+		return eq;
+	}
+	
+	
 	//Gets operator and applies to number
-	public static int GetOperator(int op, int num) {
+	public static int ParseOperator(int op, int num) {
 		switch(op) {
 		case 0:return num *4;
 		case 1:return num /4;
@@ -46,7 +68,7 @@ public class FourThought {
 		return 0;
 	}
 	//Gets operator and applies to string
-		public static String GetOperator(int op, String s) {
+		public static String ParseOperator(int op, String s) {
 			switch(op) {
 			case 0:return s + " * 4";
 			case 1:return s + " / 4";
