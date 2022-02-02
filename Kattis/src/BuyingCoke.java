001ec9cf5cae262eeecf8f7a9b4a75f8b670974d
Ian Ford <89503834+IanTFord@users.noreply.github.com>
2022-02-02 14:09:24 -0500

    Update BuyingCoke.java

--- Kattis/src/BuyingCoke.java
+++ Kattis/src/BuyingCoke.java
@@ -19,35 +19,91 @@ public class BuyingCoke {
 	}
 	//Gets Coins needed given inputs
 	private static int coinsNeeded(int cokesWanted, int c1, int c2, int c3) {
-		int coinsIn = 0;
+		int coinsIn = 0; 
+		boolean isFourBetter = false;
 		
 		//Loop for buying cokes until all cokes needed have been purchased
 		while(cokesWanted > 0) {
+			//Once out of dimes find whether prioritizing 2 nickels or 1 is better
+			if(c3 == 0)
+				return Math.min(minCoinsUsingBoth(c1, c2, cokesWanted), Math.min(minCoinsUsing4(c1, c2, cokesWanted), minCoinsUsing2(c1, c2, cokesWanted)))+ coinsIn;
 			if(c3 > 0) {
 				coinsIn++;
 				c3--;
 				c1+=2;
+				cokesWanted --;
 			}
-			//							Decides whether buying coke with all 5 coins is better than buying with 1 5 and 3 1's
-			else if(c2 > 0 && c1 > 2 &&  c2 /2 < cokesWanted && (cokesWanted - c2) * 8 + c2 < (cokesWanted - c2/2) * 8 + (c2/2) * 2 + (c2 - c2/2*2) * 4) {
-				coinsIn+=4;
-				c2-=1;
-				c1-=3;
-			}
-			else if(c2 > 1) {
-				coinsIn+=2;
-				c2-=2;
-				c1+=2;
-			}
-			else {
-				coinsIn+=8;
-				c1 -=8;
-			}
-			cokesWanted--;
 		}
 		return coinsIn;
 	}
 	
+	//Finds the number of coins spent when buying with 1 nickel and 3 pennies whenever it can
+	public static int minCoinsUsing4(int c1, int c2, int cokes){
+		int leastCoins = 0;
+		while(cokes > 0){
+			if(c2 > 0 && c1 > 2){
+				leastCoins += 4;
+				c2--;
+				c1 -=3;
+			}
+			else if(c2 > 1){
+				leastCoins += 2;
+				c2 -= 2;
+				c1 += 2;
+			}
+			else {
+				leastCoins += cokes * 8;
+				break;
+			}
+			cokes--;
+		}
+		return leastCoins;
+	}
+	
+	//Finds the number of coins spent when buying with 1 nickel and 3 pennies whenever it can
+	public static int minCoinsUsing2(int c1, int c2, int cokes){
+		int leastCoins = 0;
+		while(cokes > 0){
+			if(c2 > 1){
+				leastCoins += 2;
+				c2 -= 2;
+				c1 += 2;
+			}
+			else if(c2 > 0 && c1 > 2){
+				leastCoins += 4;
+				c2--;
+				c1 -=3;
+			}
+
+			else {
+				leastCoins += cokes * 8;
+				break;
+			}
+			cokes--;
+		}
+		return leastCoins;
+	}
+	//Finds the number of coins spent when buying with 1 nickel and 3 pennies whenever it can
+		public static int minCoinsUsingBoth(int c1, int c2, int cokes){
+			int leastCoins = 0;
+			while(cokes > 0){
+				if(c2 > 0 && c1 > 2 && c2 <= cokes){
+					leastCoins += 4;
+					c2--;
+					c1 -=3;
+				}
+				else if(c2 > 1){
+					leastCoins += 2;
+					c2 -= 2;
+					c1 += 2;
+				}
+				else {
+					leastCoins += cokes * 8;
+					break;
+				}
+				cokes--;
+			}
+			return leastCoins;
+		}
+	
 }
-
-
